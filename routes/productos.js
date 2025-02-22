5c030a9faea5790fe1dec203d3866bcc9642a2f1
Miguel López <miguel.a.lopez.m0502@gmail.com>
2025-02-22 10:28:00 +0100

    cambio:tablaproductos

--- routes/productos.js
+++ routes/productos.js
@@ -2,6 +2,7 @@ const productos=[
     {id:1, nombre:'producto1', precio:100},
     {id:2, nombre:'producto2', precio:200},
     {id:3, nombre:'producto3', precio:300},
+    {id:4, nombre:'producto4', precio:400},
 ]
 
 async function Obtenerlistaproductos() {
